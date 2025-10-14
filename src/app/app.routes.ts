401a5aff1900ef4de162fc2bce3a12cd1bb9d5f7
Yadav <2440540@cognizant.com>
2025-10-14 15:06:16 +0530

    user and admin structure added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CartPage } from './features/cart/cart-page/cart-page';
+import { HomePage } from './features/user/home/home-page/home-page';
 
 export const routes: Routes = [
-    {path: 'cart', component: CartPage}
+    {path: 'cart', component: CartPage},
+    {path: '', component: HomePage}
 ];
